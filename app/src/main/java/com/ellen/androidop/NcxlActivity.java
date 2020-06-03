3ccaed7110480f60e11745c19cbca6c27bafebf5
陈浪 <chenlang@sx985.com>
2020-06-03 14:13:42 +0800

    更新apk瘦身

--- app/src/main/java/com/ellen/androidop/NcxlActivity.java
+++ app/src/main/java/com/ellen/androidop/NcxlActivity.java
@@ -40,7 +40,6 @@ public class NcxlActivity extends AppCompatActivity {
         @Override
         public void handleMessage(@NonNull Message msg) {
             super.handleMessage(msg);
-            
             ncxlActivityWeakReference.get().a = 4;
         }
     }
